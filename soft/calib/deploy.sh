d810f180adf94d126961c8adc1d0f716f4223288
astralien3000 <astralien3000@yahoo.fr>
2014-11-01 12:49:01 +0100

    Multipwm example now compiles

--- soft/calib/deploy.sh
+++ soft/calib/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-avr-objcopy -O ihex robot_atmega2560.elf robot_atmega2560.hex
-sudo avrdude -cwiring -patmega2560 -P /dev/ttyACM0 -U flash:w:robot_atmega2560.hex -D
+BIN=calib
+
+avr-objcopy -O ihex ${BIN}_atmega2560.elf ${BIN}_atmega2560.hex
+sudo avrdude -cwiring -patmega2560 -P /dev/ttyACM0 -U flash:w:${BIN}_atmega2560.hex -D
