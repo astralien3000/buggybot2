8980d989e41f992c009d71532f10228d0d87358b
astralien3000 <astralien3000@yahoo.fr>
2016-01-10 03:10:18 +0100

    Moved DDS node initialization to a lib

--- soft/strategy/app/body_pose/body_pose_node.cpp
+++ soft/strategy/app/body_pose/body_pose_node.cpp
@@ -9,194 +9,29 @@
 
 #include <dds/DCPS/StaticIncludes.h>
 
+#include <reader_node.hpp>
+#include <writer_node.hpp>
+
 #include <buggybot_2015_mgd.hpp>
 
+DEFINE_MSG_TYPE(JointAngles, Buggybot::JointAngles)
+DEFINE_MSG_TYPE(EntityPosition, Buggybot::EntityPosition)
+
 struct BodyPoseNode::PrivateData {
-  DDS::DomainParticipantFactory_var dpf;
-  DDS::DomainParticipant_var participant;
-  
-  Buggybot::JointAnglesTypeSupport_var type_support;
-  DDS::Topic_var joint_angles_topic;
-  DDS::Subscriber_var subscriber;
-  DDS::DataReaderListener_var listener;
-  DDS::DataReader_var reader;
-
-  Buggybot::EntityPositionTypeSupport_var ep_type_support;
-  DDS::Topic_var ep_topic;
-  DDS::Publisher_var ep_publisher;
-  Buggybot::EntityPositionDataWriter_var ep_writer;
+  ReaderNode<JointAngles> ja_reader;
+  WriterNode<EntityPosition> ep_writer;
   Buggybot::EntityPosition ep_msg;
-
   Buggybot2015MGD mgd;
+
+  PrivateData(BodyPoseNode* n, int argc, char* argv[])
+    : ja_reader("test", [n](Buggybot::JointAngles msg){n->onJointAngles(msg);}, argc, argv),
+      ep_writer("test2", argc, argv) {
+
+  }
 };
-
-class DataReaderListenerImpl : public DDS::DataReaderListener {
-private:
-  BodyPoseNode& _node;
-  
-public:
-  DataReaderListenerImpl(BodyPoseNode& node)
-    : _node(node) {
-    
-  }
-  
-  void on_data_available(DDS::DataReader_ptr reader) {
-    std::cout << "data available" << std::endl;
-
-    Buggybot::JointAnglesDataReader_var test_reader =
-      Buggybot::JointAnglesDataReader::_narrow(reader);
-
-    if(!test_reader) {
-      std::cerr << "_narrow failed" << std::endl;
-      return;
-    }
-
-    Buggybot::JointAngles msg;
-    DDS::SampleInfo si;
-    DDS::ReturnCode_t status = test_reader->take_next_sample(msg, si);
-
-    if(status == DDS::RETCODE_OK) {
-      _node.onJointAngles(msg);
-    }
-    else {
-      std::cerr << "take_next_sample failed" << std::endl;
-      return;
-    }
-  }
-
-  void on_requested_deadline_missed(DDS::DataReader_ptr reader, const DDS::RequestedDeadlineMissedStatus& status) {}
-  void on_requested_incompatible_qos(DDS::DataReader_ptr reader, const DDS::RequestedIncompatibleQosStatus& status) {}
-  void on_sample_rejected(DDS::DataReader_ptr reader, const DDS::SampleRejectedStatus& status) {}
-  void on_liveliness_changed(DDS::DataReader_ptr reader, const DDS::LivelinessChangedStatus& status) {}
-  
-  void on_subscription_matched(DDS::DataReader_ptr reader, const DDS::SubscriptionMatchedStatus& status) {
-    std::cout << "match" << std::endl;
-  }
-  
-  void on_sample_lost(DDS::DataReader_ptr reader, const DDS::SampleLostStatus& status) {}
-};
-
 
 BodyPoseNode::BodyPoseNode(int argc, char* argv[]) {
-  _data = new PrivateData();
-
-  try {
-    // Initialize DomainParticipantFactory
-    _data->dpf = TheParticipantFactoryWithArgs(argc, argv);
-
-    // Create DomainParticipant
-    _data->participant =
-      _data->dpf->create_participant(42,
-                              PARTICIPANT_QOS_DEFAULT,
-                              0,
-                              OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-
-    
-    if (!_data->participant) {
-      std::cerr << "create_participant failed" << std::endl;
-      exit(-1);
-    }
-
-    // Register TypeSupport (Test)
-    _data->type_support =
-      new Buggybot::JointAnglesTypeSupportImpl;
-    
-    if(DDS::RETCODE_OK != _data->type_support->register_type(_data->participant, "")) {
-      std::cerr << "register_type failed" << std::endl;
-      exit(-1);
-    }
-
-    // Create topic
-    {
-      CORBA::String_var type_name =
-	_data->type_support->get_type_name();
-      
-      _data->joint_angles_topic =
-	_data->participant->create_topic("joint_angles", type_name, TOPIC_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-      
-      if(!_data->joint_angles_topic) {
-	std::cerr << "create_topic failed" << std::endl;
-	exit(-1);
-      }
-    }
-      
-    // Create _data->subscriber
-    _data->subscriber =
-      _data->participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-
-    if(!_data->subscriber) {
-      std::cerr << "create_subscriber failed" << std::endl;
-      exit(-1);
-    }
-
-    // Create listener
-    _data->listener = new DataReaderListenerImpl(*this);
-
-    // Create DataReader
-    _data->reader =
-      _data->subscriber->create_datareader(_data->joint_angles_topic,
-					   DATAREADER_QOS_DEFAULT,
-					   _data->listener,
-					   OpenDDS::DCPS::DEFAULT_STATUS_MASK
-					   );
-
-    if(!_data->reader) {
-      std::cerr << "create_datareader failed" << std::endl;
-      exit(-1);
-    }
-
-    // Register TypeSupport
-    _data->ep_type_support = 
-      new Buggybot::EntityPositionTypeSupportImpl;
-
-    if(DDS::RETCODE_OK != _data->ep_type_support->register_type(_data->participant, "")) {
-      std::cerr << "register_type failed" << std::endl;
-      exit(-1);
-    }
-
-    // Create topic
-    {
-      CORBA::String_var type_name =
-	_data->ep_type_support->get_type_name();
-      
-      _data->ep_topic =
-	_data->participant->create_topic("endpoint_position", type_name, TOPIC_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-      
-      if(!_data->ep_topic) {
-	std::cerr << "create_topic failed" << std::endl;
-	exit(-1);
-      }
-    }
-      
-    // Create publisher
-    _data->ep_publisher =
-      _data->participant->create_publisher(PUBLISHER_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-    
-    if(!_data->ep_publisher) {
-      std::cerr << "create_publisher failed" << std::endl;
-      exit(-1);
-    }
-
-    // Create data writer
-    {
-      DDS::DataWriter_var writer =
-	_data->ep_publisher->create_datawriter(_data->ep_topic, DATAWRITER_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-      
-      if(!writer) {
-	std::cerr << "create_datawriter failed" << std::endl;
-	exit(-1);
-      }
-      
-      // Test Writer
-      _data->ep_writer =
-	Buggybot::EntityPositionDataWriter::_narrow(writer);
-    }
-    
-  }
-  catch (const CORBA::Exception& e) {
-    e._tao_print_exception("Exception caught in BodyPodeNode constructor");
-    exit(-1);
-  }
+  _data = new PrivateData(this, argc, argv);
 }
 
 BodyPoseNode::~BodyPoseNode(void) {
@@ -233,7 +68,7 @@ void BodyPoseNode::onJointAngles(Buggybot::JointAngles msg) {
     _data->ep_msg.x = v[0];
     _data->ep_msg.y = v[1];
     _data->ep_msg.z = v[2];
-    _data->ep_writer->write(_data->ep_msg, DDS::HANDLE_NIL);
+    _data->ep_writer.write(_data->ep_msg);
   };
 
   write_entity_pos("lf", "lf.endpoint");
--- soft/strategy/app/body_pose/test/test.cpp
+++ soft/strategy/app/body_pose/test/test.cpp
@@ -12,113 +12,26 @@
 #include <dds/DCPS/StaticIncludes.h>
 
 #include "buggybotTypeSupportImpl.h"
+#include <writer_node.hpp>
 
+DEFINE_MSG_TYPE(JointAngles, Buggybot::JointAngles)
 
 int main(int argc, char* argv[]) {
-  try {
-    // Initialize DomainParticipantFactory
-    DDS::DomainParticipantFactory_var dpf =
-      TheParticipantFactoryWithArgs(argc, argv);
 
-    // Create DomainParticipant
-    DDS::DomainParticipant_var participant =
-      dpf->create_participant(42,
-                              PARTICIPANT_QOS_DEFAULT,
-                              0,
-                              OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+  WriterNode<JointAngles> node("test", argc, argv);
 
-    
-    if (!participant) {
-      std::cerr << "create_participant failed" << std::endl;
-      return -1;
-    }
-
-    // Register TypeSupport (Test)
-    Buggybot::JointAnglesTypeSupport_var ts =
-      new Buggybot::JointAnglesTypeSupportImpl;
-
-    if(DDS::RETCODE_OK != ts->register_type(participant, "")) {
-      std::cerr << "register_type failed" << std::endl;
-      return -1;
-    }
-
-    // Create topic
-    CORBA::String_var type_name = ts->get_type_name();
-
-    DDS::Topic_var topic = participant->create_topic("joint_angles", type_name, TOPIC_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-
-    if(!topic) {
-      std::cerr << "create_topic failed" << std::endl;
-      return -1;
-    }
-    
-    // Create publisher
-    DDS::Publisher_var pub = participant->create_publisher(PUBLISHER_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-    if(!pub) {
-      std::cerr << "create_publisher failed" << std::endl;
-      return -1;
-    }
-
-    // Create data writer
-    DDS::DataWriter_var writer = pub->create_datawriter(topic, DATAWRITER_QOS_DEFAULT, 0, OpenDDS::DCPS::DEFAULT_STATUS_MASK);
-    if(!writer) {
-      std::cerr << "create_datawriter failed" << std::endl;
-      return -1;
-    }
-
-    // Test Writer
-    Buggybot::JointAnglesDataWriter_var test_writer = Buggybot::JointAnglesDataWriter::_narrow(writer);
-
-    /*
-    // Block until Subscriber is available
-    DDS::StatusCondition_var condition = 
-      writer->get_statuscondition();
-    condition->set_enabled_statuses(
-				   DDS::PUBLICATION_MATCHED_STATUS);
-    DDS::WaitSet_var ws = new DDS::WaitSet;
-    ws->attach_condition(condition);
-    while (true) {
-      DDS::PublicationMatchedStatus matches;
-      if (writer->get_publication_matched_status(matches) 
-          != DDS::RETCODE_OK) {
-        std::cerr << "get_publication_matched_status failed!" 
-                  << std::endl;
-        return 1;
-      }
-      if (matches.current_count >= 1) {
-        break;
-      }
-      DDS::ConditionSeq conditions;
-      DDS::Duration_t timeout = { 60, 0 };
-      if (ws->wait(conditions, timeout) != DDS::RETCODE_OK) {
-        std::cerr << "wait failed!" << std::endl;
-        return 1;
-      }
-    }
-    ws->detach_condition(condition);
-    */
-    
-    // Publish
-    int i = 0;
-    while(1) {
+  // Publish
+  int i = 0;
+  while(1) {
       i++;
       Buggybot::JointAngles msg;
       msg.id = i;
       msg.lf0 = i*3.14/180.0;
       //msg.value = 42;
       //msg.text = "test";
-      if(test_writer->write(msg, DDS::HANDLE_NIL) != DDS::RETCODE_OK) {
-	std::cerr << "write failed" << std::endl;
-	//return -1;
-      }
-      std::cout << "write" << std::endl;
+      node.write(msg);
       usleep(1000000);
     }
-  }
-  catch (const CORBA::Exception& e) {
-    e._tao_print_exception("Exception caught in main():");
-    return -1;
-  }
-  
+
   return 0;
 }
