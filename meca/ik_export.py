ab10e84ea2b7260fc62fa7119c4d366a3ff7c494
Loïc Dauphin <astralien3000@yahoo.fr>
2016-04-09 17:10:11 +0200

    improved ik exporter

--- meca/ik_export.py
+++ meca/ik_export.py
@@ -11,20 +11,53 @@ def find(bone, func):
     func(bone)
     for b in bone.children:
         find(b, func)
-        
-def my_print(bone):
-    print("using " + str(bone.name).replace(".00", "") + " = ")
-    print("G::Matrix<4,4>::Any<")
-    for row in bone_matrix(bone):
-        for col in row:
-            p = int(col*1024)
-            q = int(1024)
-            if(p%q == 0):
-                print("  C<"+str(int(p/q))+">,")
-            else:
-                print("  Q<"+str(p)+","+str(q)+">,")
-    print(">;")
-                
-            
+      
+def real2cas(value):
+    ret  = "CAS::General<double, double>::"
+    p = int(value*1024)
+    q = int(1024)
+    if(p%q == 0):
+        ret += "C<"+str(int(p/q))+">"
+    else:
+        ret += "Q<"+str(p)+","+str(q)+">"
+    return ret
 
-find(bpy.data.armatures[0].bones[0], my_print)
+def matrix2cas(mat):
+    vals = []
+    for row in mat:
+        for case in row:
+            vals += [real2cas(case)]
+    ret  = "CAS::General<double, double>::"
+    ret += "Matrix<4,4>::"
+    ret += "Any<"
+    ret += ','.join(vals)
+    ret += ">"
+    return ret;
+
+def name2cxx(name):
+    return str(name).replace(".", "_")
+
+def print_bone(bone):
+    print("struct " + name2cxx(bone.name) + " {")
+    if bone.parent != None:
+        print("using parent = " + name2cxx(bone.parent.name) + ";")
+    else:
+        print("using parent = void;")
+    mat  = "using matrix = "
+    mat += matrix2cas(bone_matrix(bone)) + ";"
+    print(mat)
+    print("};")
+
+def print_armature(armature):
+    print("namespace " + name2cxx(armature.name) + " {")
+    for b in armature.bones:
+        print_bone(b)
+    print("}")
+
+for a in bpy.data.armatures:
+    print_armature(a)
+    
+#for o in bpy.data.objects:
+#    if(o.pose != None):
+#        for b in o.pose.bones:
+#            print(b.name)
