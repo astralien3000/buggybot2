8d73acfc10e935e81e754985702d0a2a2d113d56
Loïc Dauphin <astralien3000@yahoo.fr>
2016-04-10 17:38:12 +0200

    Implementation of RobotModel in ik app

--- soft/app/ik/main.cpp
+++ soft/app/ik/main.cpp
@@ -7,8 +7,25 @@
 
 #include <cereal/archives/json.hpp>
 
+#include "robot_model.hpp"
+
 using namespace std;
 
 int main(int, char**) {
+  RobotModel bot(0.00325, 100, 1);
+
+  //bot.setAngle(RobotModel::Leg::LF, 0, -3.1415/2);
+  bot.setEndpoint(RobotModel::Leg::LF, Matrix<double, 3,1>(-100.0, -84.8, -150.0));
+
+  cout << bot.getAngle(RobotModel::Leg::LF, 0) << endl;
+  cout << bot.getAngle(RobotModel::Leg::LF, 1) << endl;
+  cout << bot.getAngle(RobotModel::Leg::LF, 2) << endl;
+  cout << endl;
+
+  auto pos = bot.getEndpoint(RobotModel::Leg::LF);
+
+  cout << pos(0,0) << endl;
+  cout << pos(1,0) << endl;
+  cout << pos(2,0) << endl;
   return 0;
 }
--- soft/app/ik/robot_model.hpp
+++ soft/app/ik/robot_model.hpp
@@ -4,19 +4,47 @@
 #include <base/matrix.hpp>
 
 class RobotModel {
+private:
+  double _gain;
+  uint16_t _max_iter;
+  double _stop_dist;
+
 public:
   enum class Leg { LF, RF, LB, RB };
 
-  RobotModel();
+  inline RobotModel(double gain, uint16_t max_iter, double stop_dist)
+    : _gain(gain), _max_iter(max_iter), _stop_dist(stop_dist) {
+  }
 
-  double getGradientMethodGain(void);
-  void setGradientMethodGain(double gain);
+  inline double getGradientMethodGain(void) {
+    return _gain;
+  }
+
+  inline void setGradientMethodGain(double gain) {
+    _gain = gain;
+  }
+
+  inline uint16_t getGradientMethodMaxIterations(void) {
+    return _max_iter;
+  }
+
+  inline void setGradientMethodMaxIterations(uint16_t max) {
+    _max_iter = max;
+  }
+
+  inline double getGradientMethodStopDistance(void) {
+    return _stop_dist;
+  }
+
+  inline void setGradientMethodStopDistance(double dist) {
+    _stop_dist = dist;
+  }
 
   double getAngle(Leg leg, int id);
   void setAngle(Leg leg, int id, double angle);
 
   Matrix<double, 3,1> getEndpoint(Leg leg);
-  void setEndpoint(Leg leg, Matrix<double, 3,1> pos);
+  bool setEndpoint(Leg leg, Matrix<double, 3,1> pos);
 };
 
 #endif//ROBOT_MODEL_HPP
